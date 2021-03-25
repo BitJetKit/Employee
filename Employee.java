40a7358424d3e61dcf7ffa0bdcf8a56a88c9fb78
Jonathan M. Kelly <bitjetkit@gmail.com>
2021-03-24 21:26:32 -0500

    Declare the accessors

--- Employee.java
+++ Employee.java
@@ -23,9 +23,31 @@ public class Employee {
         this.department = department;
         this.age = age;
     }
-
-
     // Declare the accessors.
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDepartment() {
+        return department;
+    }
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     // Declare the printer.
 }
