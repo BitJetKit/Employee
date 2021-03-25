643e2bf853286d210dd904728703f0f8baf84d9d
Jonathan M. Kelly <bitjetkit@gmail.com>
2021-03-24 21:22:36 -0500

    Add construction

---
+++ Employee.java
@@ -0,0 +1,31 @@
+package Employee;
+/**
+ * @author:BitJetKit
+ * Title: Employee
+ * Date: March 24, 2021
+ * Purpose: Offer an employer an easy employee class: it can be called.
+ */
+public class Employee {
+    // Declare the fields.
+    private String id;
+    private String name;
+    private String department;
+    private int age;
+
+    // Declare the constructor options.
+    public Employee(){
+
+    }
+    // Get the employee's id, name, and department; and age.
+    public Employee(String id, String.name, String department){
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.age = age;
+    }
+
+
+    // Declare the accessors.
+
+    // Declare the printer.
+}
